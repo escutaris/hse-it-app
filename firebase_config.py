2a49c61e6e23512cca169fdcffd0bdb4aa5505a3
escutaris <escutarisonline@gmail.com>
2025-03-18 22:11:54 -0300

    Conectar credenciais do Firebase.

---
+++ firebase_config.py
@@ -0,0 +1,11 @@
+# firebase_config.py
+
+firebaseConfig = {
+  "apiKey": "AIzaSyC9EnMe_gq7oJrOf9Lp3mRyYKEYcqo1dGI",
+  "authDomain": "hse-app-escutaris.firebaseapp.com",
+  "projectId": "hse-app-escutaris",
+  "storageBucket": "hse-app-escutaris.firebasestorage.app",
+  "messagingSenderId": "862256191765",
+  "appId": "1:862256191765:web:93d2976d6af3f47b18711c",
+  "measurementId": "G-OHG200KT2M"
+}
